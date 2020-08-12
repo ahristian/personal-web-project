a1d18247af0ac248ff1ee13897689bf5ad0001af
ahristian <hristian.andrei@gmail.com>
2020-08-11 18:21:51 -0600

    Form added

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,12 +1,66 @@
+$(document).ready(function () {
+  $('#contact').validate({
+    debug: true,
+    errorClass: 'alert alert-danger',
+    errorLabelContainer: '#output-area',
+    errorElement: 'div',
+    //rules here define what is good for input
+    //each start with the form input element Name
+    rules: {
+      name: {
+        required: true
+      },
+      email: {
+        email: true,
+        required: true
+      },
+      message: {
+        required: true,
+        maxlength: 2000
+      },
+      phone: {
+        required: true,
+        maxlength: 16
+      }
+    },
+    //error messages
+    messages: {
+      name: {
+        required: "Name is a required field. Please provide a name."
+      },
+      email: {
+        required: "Email is a required field. Please provide a valid email.",
+        email: "Please provide valid email address."
+      },
+      phone: {
+        required: "Phone is a required field. Please provide a valid phone number.",
+        maxlength: "Phone number is too long. Please check your input."
+      },
+      message: {
+        required: "Message is a required field. Please type some information.",
+        maxlength: "Message is too long. Please provide a shorter message."
+      },
 
-function getInfoForm(event) {
-    event.preventDefault()
-    let nameInput = document.getElementById("name-input").value;
-    let phoneInput = document.getElementById("phone-input").value;
-    let emailInput = document.getElementById("email-input").value;
-    let messageInput = document.getElementById("message-input").value;
-    console.log("Name: ", nameInput);
-    console.log("Phone: ", phoneInput);
-    console.log("Email: ", emailInput);
-    console.log("Message: ", messageInput);
-}
+    },
+    submitHandler: function (form) {
+      $("#contact").ajaxSubmit({
+        type: "POST",
+        url: $('#contact').attr('action'),
+        success: function (ajaxOutput) {
+          $("#output-area").css("display", "")
+          $("#output-area").html(ajaxOutput)
+
+          if($(".alert-success").length >= 1) {
+            $("#contact")[0].reset()
+
+          }
+
+        }
+
+      })
+
+    }
+
+  })
+
+})
